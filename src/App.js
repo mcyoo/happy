8c5d7e1cb3b7b1d1bd183b14608df5c7e458b131
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-07-27 23:46:41 +0900

    art_from_junghwa3

--- src/App.js
+++ src/App.js
@@ -14,18 +14,25 @@ class App extends React.Component {
     this.openBottomSheet(!this.state.open);
   }
   openBottomSheet(open) {
-    this.setState({ open, message: [] });
-    clearInterval(this.state.intervalID);
-    this.setIntervel_func("희망");
+    this.setState({ open });
+
+    if (open === false && this.state.message[0] === "불안") {
+      this.setState({ message: [] });
+      clearInterval(this.state.intervalID);
+      this.setIntervel_func("희망");
+    }
+    if (open === false && this.state.message[0] === "희망") {
+      this.setState({ message: [] });
+      clearInterval(this.state.intervalID);
+      this.setIntervel_func("불안");
+    }
   }
 
   setIntervel_func(word) {
-    const timeoutID = setTimeout(() => {
-      const intervalID = setInterval(() => {
-        var message = this.state.message;
-        message.push(word);
-        this.setState({ message, intervalID });
-      }, 1000);
+    const intervalID = setInterval(() => {
+      var message = this.state.message;
+      message.push(word);
+      this.setState({ message, intervalID });
     }, 1000);
   }
 
@@ -38,13 +45,13 @@ class App extends React.Component {
     return (
       <div className="container mx-auto">
         {message[0] === "희망" ? (
-          <div className="text-right mr-24 text-lg text-blue-700">
+          <div className="text-right mr-20 text-2xl font-bold text-blue-600">
             {message.map((message) => (
               <div>{message}</div>
             ))}
           </div>
         ) : (
-          <div className="ml-24 text-lg">
+          <div className="ml-20 text-2xl font-bold">
             {message.map((message) => (
               <div>{message}</div>
             ))}
@@ -73,18 +80,20 @@ class App extends React.Component {
               />
             )}
           </div>
-          <div className="h-screen w-screen break-words p-10 text-gray-800 text-sm">
-            누나는 초단위로 불안을 느꼈다고 했다. 같이 힘든 시기를 견뎠던
-            동료들이 코로나 때문에 뿔뿔히 흩어졌다. 누나가 일해왔던 서비스직은
-            더 이상 사람을 뽑지 않았다. 코로나로 인해 기쁨과 희망이 무너져
-            내렸다. 과거 현재 미래는 초 단위로 오는 불안함으로 채워진다.
-            이야기를 듣는 나는 어제의 나 와 마찬가지로 할 수 있는게 없었다. 감히
-            공감 조차 할 수 없었다. 다만 절망의 늪에 빠져있지 말고, 밖에 좋은 곳
-            있으니 우리 손잡고 같이 가보자 라는 기대를 심어주고 싶었다. 빛과
-            어둠이 공존하듯, 슬픔,절망 도 기쁨과 희망이 같이 있는거라고, 한쪽에
-            너무 빠져 있지말고 외면하고 당위로 다가오는 습관에 굴레에서 벗어나
-            반대쪽도 있다고 나는 그저 말해주고 싶었다. 누나에게 이 작품을
-            바친다.
+          <div className="h-screen w-screen p-10 text-gray-800 text-sm">
+            <p className="break-words">
+              누나는 초단위로 불안을 느꼈다고 했다. 같이 힘든 시기를 견뎠던
+              동료들이 코로나 때문에 뿔뿔히 흩어졌다. 누나가 일해왔던 서비스직은
+              더 이상 사람을 뽑지 않았다. 코로나로 인해 기쁨과 희망이 무너져
+              내렸다. 과거 현재 미래는 초 단위로 오는 불안함으로 채워진다.
+              이야기를 듣는 나는 어제의 나 와 마찬가지로 할 수 있는게 없었다.
+              감히 공감 조차 할 수 없었다. 다만 절망의 늪에 빠져있지 말고, 밖에
+              좋은 곳 있으니 우리 손잡고 같이 가보자 라는 기대를 심어주고
+              싶었다. 빛과 어둠이 공존하듯, 슬픔,절망 도 기쁨과 희망이 같이
+              있는거라고, 한쪽에 너무 빠져 있지말고 외면하고 당위로 다가오는
+              습관에 굴레에서 벗어나 반대쪽도 있다고 나는 그저 말해주고 싶었다.
+              누나에게 이 작품을 바친다.
+            </p>
           </div>
         </SwipeableBottomSheet>
       </div>
